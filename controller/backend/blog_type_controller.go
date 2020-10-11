948b9c779d06ca9182a000a4eda962af9eaa7b5c
FlashFeiFei <51785816@qq.com>
2020-10-11 18:44:59 +0800

    默认值

--- controller/backend/blog_type_controller.go
+++ controller/backend/blog_type_controller.go
@@ -15,8 +15,8 @@ func (c *BlogTypeController) GetList() {
 
 	//必填字段
 	type searchRequest struct {
-		PerPage int `form:"per_page" binding:"required"`
-		Page    int `form:"page" binding:"required"`
+		PerPage int `form:"per_page"`
+		Page    int `form:"page"`
 	}
 
 	var search_request searchRequest
@@ -26,6 +26,14 @@ func (c *BlogTypeController) GetList() {
 	if err != nil {
 		help.Gin200ErrorResponse(c.Ctx, exception.VALIDATE_ERR, err.Error(), nil)
 		return
+	}
+
+	//参数默认值
+	if search_request.PerPage <= 0 {
+		search_request.PerPage = 10
+	}
+	if search_request.Page <= 0 {
+		search_request.Page = 1
 	}
 
 	b_t_s := new(backend.BlogTypeService)
