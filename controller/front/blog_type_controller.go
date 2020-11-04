5fc3a2b6d94455fd57a25f9e5d2722ad66ae4a10
qixi <qixi@beihai365.com>
2020-11-04 15:55:50 +0800

    整理一下

--- controller/front/blog_type_controller.go
+++ controller/front/blog_type_controller.go
@@ -1,5 +1,45 @@
 package front
 
+import (
+	"github.com/blog_backend/exception"
+	"github.com/blog_backend/help"
+	"github.com/blog_backend/service/blog"
+)
+
 type BlogTypeController struct {
 	BaseController
+}
+
+/**
+获取博客类型列表
+*/
+func (c *BlogTypeController) GetList() {
+	//必填字段
+	type searchRequest struct {
+		PerPage int `form:"per_page"`
+		Page    int `form:"page"`
+	}
+
+	var search_request searchRequest
+
+	err := c.Ctx.ShouldBind(&search_request)
+
+	if err != nil {
+		help.Gin200ErrorResponse(c.Ctx, exception.VALIDATE_ERR, err.Error(), nil)
+		return
+	}
+
+	//参数默认值
+	if search_request.PerPage <= 0 {
+		search_request.PerPage = 10
+	}
+	if search_request.Page <= 0 {
+		search_request.Page = 1
+	}
+
+	service := new(blog.BlogTypeBkService)
+	result := service.GetList(search_request.PerPage, search_request.Page)
+
+	help.Gin200SuccessResponse(c.Ctx, "成功", result)
+	return
 }
