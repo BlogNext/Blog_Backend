4bb6297368bc8aba2169eb915f032e51b28e09f4
qixi <qixi@beihai365.com>
2020-11-06 11:36:19 +0800

    update

--- controller/backend/blog_controller.go
+++ controller/backend/blog_controller.go
@@ -4,14 +4,12 @@ import (
 	"github.com/blog_backend/exception"
 	"github.com/blog_backend/help"
 	"github.com/blog_backend/service/blog"
+	"strings"
 )
 
 type BlogController struct {
 	BaseController
 }
-
-
-
 
 //导入数据到es中
 func (c *BlogController) ImportData() {
@@ -28,10 +26,39 @@ func (c *BlogController) ImportData() {
 		return
 	}
 
-	b_s := new(blog.BlogBkService)
+	if strings.Compare(import_request.Password, "ly123") != 0 {
+		help.Gin200ErrorResponse(c.Ctx, exception.VALIDATE_ERR, "密码不对", nil)
+		return
+	}
+
+	b_s := new(blog.BlogEsBkService)
 	b_s.ImportDataToEs()
 
 	help.Gin200SuccessResponse(c.Ctx, "导入完毕", nil)
 	return
 }
 
+func (c *BlogController) CreateIndex() {
+	type importRequest struct {
+		Password string `form:"password" binding:"required"`
+	}
+
+	var import_request importRequest
+
+	err := c.Ctx.ShouldBind(&import_request)
+	if err != nil {
+		help.Gin200ErrorResponse(c.Ctx, exception.VALIDATE_ERR, "不要乱动这个方法，这个方法不对外提供的，请联系ly", nil)
+		return
+	}
+
+	if strings.Compare(import_request.Password, "ly123") != 0 {
+		help.Gin200ErrorResponse(c.Ctx, exception.VALIDATE_ERR, "密码不对", nil)
+		return
+	}
+
+	b_s := new(blog.BlogEsBkService)
+	b_s.CreateIndex()
+
+	help.Gin200SuccessResponse(c.Ctx, "创建完毕", nil)
+	return
+}
--- service/blog/blog_es_bk.go
+++ service/blog/blog_es_bk.go
@@ -1,7 +1,11 @@
 package blog
 
 import (
+	"fmt"
+	"github.com/blog_backend/common-lib/db/mysql"
 	"github.com/blog_backend/entity/blog"
+	"github.com/blog_backend/exception"
+	"github.com/blog_backend/model"
 	"github.com/blog_backend/service/es"
 	"github.com/olivere/elastic/v7"
 	"log"
@@ -22,6 +26,43 @@ func (b *BlogEsBkService) CreateIndex() {
 		log.Println("无法创建索引")
 		panic(err)
 	}
+}
+
+//导入数据到es中
+func (s *BlogEsBkService) ImportDataToEs() {
+
+	var blog_list []model.BlogModel
+
+	db := mysql.GetDefaultDBConnect()
+	db.Find(&blog_list)
+
+	if blog_list == nil {
+		return
+	}
+
+	log.Println(fmt.Sprintf("v = %v,t = %T, p = %p", blog_list, blog_list, blog_list))
+
+	for _, blog_model := range blog_list {
+		//es中添加文件
+		blog_doc := ChangeToBlogEntity(&blog_model)
+
+		log.Println("导入的es文档是：", fmt.Sprintf("v = %v,t = %T, p = %p", blog_doc, blog_doc, blog_doc))
+
+		es_blog_service := new(BlogEsBkService)
+
+		log.Println("连接:es成功")
+
+		doc := es_blog_service.AddDoc(blog_doc)
+
+		blog_model.DocID = doc.Id
+
+		db_error := db.Save(blog_model)
+
+		if db_error.Error != nil {
+			panic(exception.NewException(exception.DATA_BASE_ERROR_EXEC, fmt.Sprintf("更新失败error:%s", db_error.Error.Error())))
+		}
+	}
+
 }
 
 //删除blog文档
