0e3fecb635cd4dec118193720ab9e030906672d8
qixi <qixi@beihai365.com>
2020-11-12 18:45:28 +0800

    正式环境关闭sql打印日志

--- common-lib/db/mysql/mysql.go
+++ common-lib/db/mysql/mysql.go
@@ -35,9 +35,9 @@ func InitDBConnect(db_info map[string]db.DBInfo) {
 		newLogger = logger.New(
 			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 			logger.Config{
-				SlowThreshold: 5 * time.Second, // 慢 SQL 阈值
-				LogLevel:      logger.Info,     // Log level
-				Colorful:      true,           // 禁用彩色打印
+				SlowThreshold: 4 * time.Second, // 慢 SQL 阈值
+				LogLevel:      logger.Error,    // Log level, 生产环境下，超过阈值的sql开启慢查询
+				Colorful:      true,            // 禁用彩色打印
 			},
 		)
 	} else {
@@ -46,17 +46,17 @@ func InitDBConnect(db_info map[string]db.DBInfo) {
 		newLogger = logger.New(
 			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 			logger.Config{
-				SlowThreshold: time.Minute, // 慢 SQL 阈值
-				LogLevel:      logger.Info, // Log level
-				Colorful:      true,        // 禁用彩色打印
+				SlowThreshold: time.Second,   // 慢 SQL 阈值
+				LogLevel:      logger.Silent, // Log level  warn和error级别下，slowthreshold才生效，非正式环境下开启sql打印
+				Colorful:      true,          // 禁用彩色打印
 			},
 		)
 	}
 
 	//主数据库
 	myGdb, err := gorm.Open(gmysql.Open(db_info["sources"].Dsn), &gorm.Config{
-		Logger: newLogger,
-		PrepareStmt:true,
+		Logger:      newLogger,
+		PrepareStmt: true,
 	})
 
 	if err != nil {
