81830c211af7499400f84d5d6d0d8c638fd94bd5
qixi <qixi@beihai365.com>
2021-03-26 15:55:06 +0800

    博客类型缓存修改

--- service/blog/blog_type_rt.go
+++ service/blog/blog_type_rt.go
@@ -1,11 +1,12 @@
 package blog
 
 import (
-	"encoding/json"
 	"github.com/blog_backend/common-lib/db/mysql"
+	"github.com/blog_backend/common-lib/db/mysql/my_db_proxy"
 	"github.com/blog_backend/entity"
 	"github.com/blog_backend/entity/blog"
 	"github.com/blog_backend/model"
+	"gorm.io/gorm"
 	"strings"
 	"time"
 )
@@ -51,64 +52,83 @@ func (s *BlogTypeRtService) getListByids(ids []uint64) (result map[uint64]*blog.
 
 //获取分类列表
 func (s *BlogTypeRtService) GetList(perPage, page int) (result *entity.ListResponseEntity) {
-	db := mysql.GetNewDB(false)
-	dbDryRun := mysql.GetNewDB(true)
 
-	var count int64
-	db.Model(&model.BlogTypeModel{})
-	dbDryRun.Model(&model.BlogTypeModel{})
+	myDBProxy := my_db_proxy.NewMyDBProxy()
 
-	statement := dbDryRun.Count(&count).Statement
-	cacheKey := "blog_type_list" + dbDryRun.Dialector.Explain(statement.SQL.String(),statement.Vars...)
-	//如果存在缓存，先从缓冲中取
-	lruCacheList, ok := BlgLruUnsafety.Get(cacheKey)
-	if ok {
-		//有缓存
-		//构建结果返回
-		result = new(entity.ListResponseEntity)
-		json.Unmarshal(lruCacheList.([]uint8),result)
+	//表名
+	myDBProxy.ExecProxy(func(db *gorm.DB, dbDryRun *gorm.DB) {
+		//需要改变一下db的内存值，gorm的clone值的问题
+		*db = *db.Model(&model.BlogTypeModel{})
+		*dbDryRun = *dbDryRun.Model(&model.BlogTypeModel{})
+	})
+
+	//返回结果
+	result = new(entity.ListResponseEntity)
+	//是否存在缓存
+	var existCache bool
+	cachePreFix := "blogTypeList_"
+	var cacheKey string
+
+	myDBProxy.ExecProxy(func(db *gorm.DB, dbDryRun *gorm.DB) {
+
+		//如果存在缓存，先从缓冲中取
+		dbDryRun.Select("id, yuque_name, yuque_id, created_at, updated_at").
+			Limit(perPage).Offset((page - 1) * perPage).Find(nil)
+
+		cacheKey = myDBProxy.BuildCacheKey(cachePreFix)
+
+		resultCache, ok := BlgLruUnsafety.Get(cacheKey)
+
+		if ok {
+			//有缓存,直接返回缓存对象
+			result = resultCache.(*entity.ListResponseEntity)
+			existCache = true
+			return
+		}
+
+		//没有缓存，获取数据集
+		rows, _ := db.Select("id, yuque_name, yuque_id, created_at, updated_at").
+			Limit(perPage).Offset((page - 1) * perPage).Rows()
+
+		blogTypeModelList := make([]map[string]interface{}, 0)
+
+		for rows.Next() {
+
+			var id int64
+			var yuqueName string
+			var yuqueId int64
+			var createdAt int64
+			var updatedAt int64
+			rows.Scan(&id, &yuqueName, &yuqueId, &createdAt, &updatedAt)
+
+			item := make(map[string]interface{})
+
+			item["id"] = id
+			item["yuque_name"] = yuqueName
+			item["yuque_id"] = yuqueId
+			item["create_time"] = createdAt
+			item["update_time"] = updatedAt
+
+			blogTypeModelList = append(blogTypeModelList, item)
+		}
+
+		result.SetList(blogTypeModelList)
+	})
+
+	if existCache {
+		//存在缓存直接返回
 		return result
 	}
 
-	db.Count(&count)
+	//没有缓存的情况下，继续计算count值，然后设置count
+	myDBProxy.ExecProxy(func(db *gorm.DB, dbDryRun *gorm.DB) {
+		var count int64
+		db.Count(&count)
+		result.SetCount(count)
+		result.SetPerPage(perPage)
+	})
 
-	rows, _ := db.Select("id, yuque_name, yuque_id, created_at, updated_at").
-		Limit(perPage).Offset((page - 1) * perPage).Rows()
-
-	defer rows.Close()
-
-	blogTypeModelList := make([]map[string]interface{}, 0)
-
-	for rows.Next() {
-
-		var id int64
-		var yuqueName string
-		var yuqueId int64
-		var createdAt int64
-		var updatedAt int64
-		rows.Scan(&id, &yuqueName, &yuqueId, &createdAt, &updatedAt)
-
-		item := make(map[string]interface{})
-
-		item["id"] = id
-		item["yuque_name"] = yuqueName
-		item["yuque_id"] = yuqueId
-		item["create_time"] = createdAt
-		item["update_time"] = updatedAt
-
-		blogTypeModelList = append(blogTypeModelList, item)
-	}
-
-	result = new(entity.ListResponseEntity)
-	result.SetCount(count)
-	result.SetPerPage(perPage)
-	result.SetList(blogTypeModelList)
-
-
-
-	//加入缓存
-	jsonCache,_ := json.Marshal(result)
-	BlgLruUnsafety.Add(cacheKey, jsonCache, 5*time.Minute)
+	BlgLruUnsafety.Add(cacheKey, result, 5*time.Minute)
 
 	return
 }
